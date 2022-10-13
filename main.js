6dfdeee941fd6b9d6b3e5dc905742b6a63589761
clobochar <clo.bochar_@hotmail.com>
2022-10-13 11:35:59 +0100

    Refactoring the code

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-let toDoList = ["Pay water bill", "Buy milk", "Take the trash"];
+let elementsToDoList = ["Pay water bill", "Buy milk", "Take the trash"];
 
-//* Create new task
+const toDoList = document.querySelector(".todo-list");
+
+//* Create initial list
+function createLi(liText) {
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.innerText = liText;
+  toDoList.appendChild(li);
+
+  //* Delete button
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.classList.add("delete-btn");
+  deleteButton.onclick = function () {
+    this.parentNode.remove();
+  };
+  li.appendChild(deleteButton);
+}
+
+elementsToDoList.forEach((item) => {
+  createLi(item);
+});
+
+//* Create new task and validate
+
 document.querySelector(".add-btn").onclick = function () {
   if (document.querySelector("input").value.length == 0) {
     alert("Please enter a task");
   } else {
-    document.querySelector(".todo-list").innerHTML += `<li class="todo-item">${
-      document.querySelector(".input").value
-    }</li><button class="delete-btn">Delete</button>`;
-
-    //* Delete the task
-    var listTasks = document.querySelectorAll(".delete-btn");
-    for (let i = 0; i < listTasks.length; i++) {
-      listTasks[i].onclick = function () {
-        this.parentNode.remove();
-      };
-    }
+    const value = document.querySelector(".input").value;
+    createLi(value);
   }
 };
